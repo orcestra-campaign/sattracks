def75c2830454c319e030a5fed724846e75b2f1c
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2024-08-06 08:49:18 +0200

    use Pandas to generate CSV version of the index

--- scripts/build_index.py
+++ scripts/build_index.py
@@ -1,24 +1,38 @@
-import glob
 import re
 from pathlib import Path
 from collections import defaultdict
 import jinja2
+import pandas as pd
+import datetime
 
 srcdir = Path(__file__).parent
 rootdir = srcdir.parent
 templatedir = srcdir / "templates"
 file_re = re.compile("PERCUSION_ORBIT_FCST_(?P<sat>[^_]+)_ORBLTP_CAPE_VERDE_ROI_V(?P<day>[0-9]{8})_F(?P<forecast_day>[0-9]{8}).txt")
 
+def get_predictions(rootdir):
+    for fn in rootdir.glob("**/*.txt"):
+        if m := file_re.match(fn.name):
+            entry = {
+                "sat": m.group("sat"),
+                "forecast_day": datetime.datetime.strptime(m.group("forecast_day"), "%Y%m%d").date(),
+                "valid_day": datetime.datetime.strptime(m.group("day"), "%Y%m%d").date(),
+                "forecast_file": fn.relative_to(rootdir),
+            }
+            kml_candidate = fn.with_suffix(".kml")
+            if kml_candidate.exists():
+                entry["kml_file"] = kml_candidate.relative_to(rootdir)
+            yield entry
+
+
 def main():
     env = jinja2.Environment(
             loader=jinja2.FileSystemLoader(templatedir),
             autoescape=jinja2.select_autoescape()
         )
-    avail = defaultdict(lambda: defaultdict(list))
-    for fn in glob.glob(str(rootdir / "**/*.txt")):
-        if m := file_re.match(fn.split("/")[-1]):
-            avail[m.group("sat")][m.group("forecast_day")].append(m.group("day"))
-    avail = {k: {**v} for k, v in avail.items()}
+
+    avail = pd.DataFrame.from_records(get_predictions(rootdir))
+    avail.to_csv(rootdir / "index.csv", index=False)
 
     template = env.get_template("index.html")
     with open(rootdir / "index.html", "w") as outfile:
