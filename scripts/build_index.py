74747d9dae372b1b11fff72c961ced353ece433a
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2024-08-12 21:47:57 +0200

    add forecast kind to index

--- scripts/build_index.py
+++ scripts/build_index.py
@@ -8,7 +8,7 @@ import datetime
 srcdir = Path(__file__).parent
 rootdir = srcdir.parent
 templatedir = srcdir / "templates"
-file_re = re.compile("PERCUSION_ORBIT_FCST_(?P<sat>[^_]+)_ORBLTP_CAPE_VERDE_ROI_V(?P<day>[0-9]{8})_F(?P<forecast_day>[0-9]{8}).txt")
+file_re = re.compile("PERCUSION_ORBIT_FCST_(?P<sat>[^_]+)_ORB(?P<kind>[A-Z]+)_CAPE_VERDE_ROI_V(?P<day>[0-9]{8})_F(?P<forecast_day>[0-9]{8}).txt")
 
 def get_predictions(rootdir):
     for fn in rootdir.glob("**/*.txt"):
@@ -17,6 +17,7 @@ def get_predictions(rootdir):
                 "sat": m.group("sat"),
                 "forecast_day": datetime.datetime.strptime(m.group("forecast_day"), "%Y%m%d").date(),
                 "valid_day": datetime.datetime.strptime(m.group("day"), "%Y%m%d").date(),
+                "kind": m.group("kind"),
                 "forecast_file": fn.relative_to(rootdir),
             }
             kml_candidate = fn.with_suffix(".kml")
