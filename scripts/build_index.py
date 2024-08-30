7f4970343ea8648f5ac4bbefe31a82bcb50447ad
Tobias Kölling <tobias.koelling@mpimet.mpg.de>
2024-08-30 22:18:44 -0100

    index entries for EXTENDED ROI

--- scripts/build_index.py
+++ scripts/build_index.py
@@ -8,23 +8,30 @@ import datetime
 srcdir = Path(__file__).parent
 rootdir = srcdir.parent
 templatedir = srcdir / "templates"
-file_re = re.compile("PERCUSION_ORBIT_FCST_(?P<sat>[^_]+)_ORB(?P<kind>[A-Z]+)_(?P<roi>[A-Z_]+)_ROI_V(?P<day>[0-9]{8})_F(?P<forecast_day>[0-9]{8}).txt")
+file_re = re.compile("PERCUSION_ORBIT_FCST_(?P<sat>[^_]+)_ORB(?P<kind>[A-Z]+)_(?P<roi>[A-Z_]+)_ROI(?:_(?P<extra_roi>[A-Z]+))?_V(?P<day>[0-9]{8})_F(?P<forecast_day>[0-9]{8}).txt")
 
 def get_predictions(rootdir):
-    for fn in rootdir.glob("**/*.txt"):
+    for fn in rootdir.glob("20*/**/*.txt"):
         if m := file_re.match(fn.name):
+            roi = m.group("roi")
+            if extra_roi := m.group("extra_roi"):
+                roi += "_" + extra_roi
+
             entry = {
                 "sat": m.group("sat"),
                 "forecast_day": datetime.datetime.strptime(m.group("forecast_day"), "%Y%m%d").date(),
                 "valid_day": datetime.datetime.strptime(m.group("day"), "%Y%m%d").date(),
                 "kind": m.group("kind"),
-                "roi": m.group("roi"),
+                "roi": roi,
                 "forecast_file": fn.relative_to(rootdir),
             }
             kml_candidate = fn.with_suffix(".kml")
             if kml_candidate.exists():
                 entry["kml_file"] = kml_candidate.relative_to(rootdir)
             yield entry
+        else:
+            fn_rel = fn.relative_to(rootdir)
+            print(f"WARNING: couldn't match pattern for file {fn_rel}")
 
 
 def main():
